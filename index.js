90f1404284851d28d5cd5e8e4dc89cc6d646c60d
Mohamed Osoble <ossoble360@gmail.com>
2023-10-03 16:40:35 -0400

    magic functiions, clear up last two tests got confused when asked to set a function as a parameter

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+returnFirstTwoDrivers = function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function returnLastTwoDrivers(drivers) {
+return drivers.slice(2,4)
+}
+let stuff = returnFirstTwoDrivers
+const selectingDrivers = [ returnFirstTwoDrivers , returnLastTwoDrivers]
+const createFareMultiplier = function createFareMultiplier(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }
+  const fareDoubler = function fareDoubler (fare) {
+    return fare * 2
+  }
+  const fareTripler = function fareTripler(fares){
+    return fares * 3
+  }
+  
+  const selectDifferentDrivers = function selectDifferentDrivers(arrayOfDrivers, functionA) {
+    return functionA(arrayOfDrivers)
+  }
+
+  
+console.log(selectDifferentDrivers(drivers,returnLastTwoDrivers ))
